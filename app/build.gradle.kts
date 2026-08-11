59b989a833f4dbefbc683312e55fe7aecb0a5a3f
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-12 02:37:57 +0300

    update `build.gradle.kts`: - add debug buildType config - upgrade java version to 17 - enable buildConfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
+
         release {
             optimization {
                 enable = false
@@ -29,12 +34,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
