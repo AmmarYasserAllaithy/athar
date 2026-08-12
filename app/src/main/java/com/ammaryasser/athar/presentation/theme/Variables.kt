2e812e3ddf4e6fc8a0a823c703cb446d445055b7
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-12 22:46:39 +0300

    add Variables.kt

---
+++ app/src/main/java/com/ammaryasser/athar/presentation/theme/Variables.kt
@@ -0,0 +1,20 @@
+package com.ammaryasser.athar.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+
+object Variables {
+    val Radius100 = 4.dp
+    val Radius200 = 8.dp
+    val Radius300 = 12.dp
+    val Radius400 = 16.dp
+    val Radius500 = 20.dp
+    val Radius600 = 24.dp
+
+    val Space100 = 4.dp
+    val Space200 = 8.dp
+    val Space300 = 12.dp
+    val Space400 = 16.dp
+    val Space500 = 20.dp
+    val Space600 = 24.dp
+}
