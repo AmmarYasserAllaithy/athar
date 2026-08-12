1084629034c2628dbce31faa345cb566c65064ee
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-12 05:53:26 +0300

    add kotlin serialization dependency and apply its plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -58,6 +59,9 @@ dependencies {
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
+
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
 
     // Koin
     implementation(libs.koin.android)
