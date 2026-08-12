ae072c93b8e10dcff117fd9700b755da67a86595
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-12 05:56:18 +0300

    introduce `AppNavHost`: - integrate in `MainActivity`

---
+++ app/src/main/java/com/ammaryasser/athar/presentation/navigation/AppNavHost.kt
@@ -0,0 +1,21 @@
+package com.ammaryasser.athar.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoute.SplashRoute,
+        modifier = modifier,
+    ) {
+    }
+}
