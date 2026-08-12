8864f5ebb6e05fa7f88c1b71a707daca2ed3ccbe
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-12 04:15:02 +0300

    theme: add Color, Type, Theme files

---
+++ app/src/main/java/com/ammaryasser/athar/presentation/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.ammaryasser.athar.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val DeepForestGreen = Color(0xFF233F37)
+val SageGreen = Color(0xFF8E9C74)
+val WarmIvory = Color(0xFFF7F2E9)
+
+val Surface = Color(0xFFFFFCF8)
+val Divider = Color(0xFFD9D2C5)
+val Accent = Color(0xFFC9A86A)
+val TextPrimary = Color(0xFF233F37)
+val TextSecondary = Color(0xFF6B7368)
