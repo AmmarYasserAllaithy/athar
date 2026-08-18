4f56ea2c6cb315d1d92987d4f0e8446125f54859
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-19 01:56:23 +0300

    apply `IBM Plex Sans Arabic` font to `Athar` theme: - add `ibm_plex_sans_arabic.ttf` font resource - implement `Typography.applyDefaultFont` extension function in `Type.kt` to apply a custom font family to all text styles - apply `IBM Plex Sans Arabic` as the default font in `Theme.kt`

--- app/src/main/java/com/ammaryasser/athar/presentation/theme/Type.kt
+++ app/src/main/java/com/ammaryasser/athar/presentation/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.ammaryasser.athar.presentation.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -32,4 +33,33 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+
+@Composable
+fun Typography.applyDefaultFont(fontFamily: FontFamily) = copy(
+    displayLarge = displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = titleLarge.copy(
+        fontFamily = fontFamily,
+        fontSize = 22.sp,
+    ),
+    titleMedium = titleMedium.copy(
+        fontFamily = fontFamily,
+        fontSize = 20.sp,
+    ),
+    titleSmall = titleSmall.copy(
+        fontFamily = fontFamily,
+        fontSize = 18.sp,
+    ),
+    bodyLarge = bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = labelSmall.copy(fontFamily = fontFamily),
 )
