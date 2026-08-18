1963002236b12860f8876f0a8d2e4602aae89999
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-19 01:41:31 +0300

    configure Ktor HttpClient and add `VersionApiService`: - provide `HttpClient` with OkHttp engine, JSON serialization, and logging in `AtharModule.kt` - implement `VersionApiService` to fetch version data from GitHub Gist - register `VersionApiService` in Koin module

--- app/src/main/java/com/ammaryasser/athar/di/AtharModule.kt
+++ app/src/main/java/com/ammaryasser/athar/di/AtharModule.kt
@@ -1,9 +1,57 @@
 package com.ammaryasser.athar.di
 
+import com.ammaryasser.athar.BuildConfig
+import com.ammaryasser.athar.data.remote.VersionApiService
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.accept
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 import org.koin.dsl.module
 
 
+@OptIn(ExperimentalSerializationApi::class)
 val atharModule = module {
 
+    // Network
+    single {
+        HttpClient(OkHttp) {
+            install(DefaultRequest) {
+                accept(ContentType.Application.Json)
+            }
+
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                    prettyPrint = true
+                    explicitNulls = false
+                    namingStrategy = JsonNamingStrategy.SnakeCase
+                }, contentType = ContentType.Any)
+            }
+
+            install(Logging) {
+                logger = Logger.ANDROID
+                level = if (BuildConfig.DEBUG) LogLevel.ALL else LogLevel.NONE
+            }
+
+            install(HttpTimeout) {
+                requestTimeoutMillis = 30_000
+                connectTimeoutMillis = 15_000
+                socketTimeoutMillis = 30_000
+            }
+        }
+    }
+    single { VersionApiService(client = get()) }
 
 }
