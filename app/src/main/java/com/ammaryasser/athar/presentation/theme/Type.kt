3d88bd1865b5b00177e99147ae75d57ff3012433
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-19 07:39:19 +0300

    update `applyDefaultFont` in `Type.kt`: - set `textDirection` to `TextDirection.Content` for all typography styles

--- app/src/main/java/com/ammaryasser/athar/presentation/theme/Type.kt
+++ app/src/main/java/com/ammaryasser/athar/presentation/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.sp
 
 
@@ -38,28 +39,67 @@ val Typography = Typography(
 
 @Composable
 fun Typography.applyDefaultFont(fontFamily: FontFamily) = copy(
-    displayLarge = displayLarge.copy(fontFamily = fontFamily),
-    displayMedium = displayMedium.copy(fontFamily = fontFamily),
-    displaySmall = displaySmall.copy(fontFamily = fontFamily),
-    headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
-    headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
-    headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
+    displayLarge = displayLarge.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    displayMedium = displayMedium.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    displaySmall = displaySmall.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    headlineLarge = headlineLarge.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    headlineMedium = headlineMedium.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    headlineSmall = headlineSmall.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
     titleLarge = titleLarge.copy(
         fontFamily = fontFamily,
         fontSize = 22.sp,
+        textDirection = TextDirection.Content,
     ),
     titleMedium = titleMedium.copy(
         fontFamily = fontFamily,
         fontSize = 20.sp,
+        textDirection = TextDirection.Content,
     ),
     titleSmall = titleSmall.copy(
         fontFamily = fontFamily,
         fontSize = 18.sp,
+        textDirection = TextDirection.Content,
     ),
-    bodyLarge = bodyLarge.copy(fontFamily = fontFamily),
-    bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
-    bodySmall = bodySmall.copy(fontFamily = fontFamily),
-    labelLarge = labelLarge.copy(fontFamily = fontFamily),
-    labelMedium = labelMedium.copy(fontFamily = fontFamily),
-    labelSmall = labelSmall.copy(fontFamily = fontFamily),
+    bodyLarge = bodyLarge.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    bodyMedium = bodyMedium.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    bodySmall = bodySmall.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    labelLarge = labelLarge.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    labelMedium = labelMedium.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
+    labelSmall = labelSmall.copy(
+        fontFamily = fontFamily,
+        textDirection = TextDirection.Content,
+    ),
 )
