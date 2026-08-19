82206bf46ef65afc7de212dccb547151bc3694b5
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-19 07:39:39 +0300

    add `imePadding` to `Scaffold` in `MainActivity.kt`

--- app/src/main/java/com/ammaryasser/athar/MainActivity.kt
+++ app/src/main/java/com/ammaryasser/athar/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -21,8 +22,12 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AtharTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavHost(modifier = Modifier.padding(innerPadding))
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .imePadding()
+                ) {
+                    AppNavHost(modifier = Modifier.padding(it))
                 }
             }
         }
