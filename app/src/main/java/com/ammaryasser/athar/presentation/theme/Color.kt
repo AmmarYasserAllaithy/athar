9eb08207bca91697702f006589f020aa6283758e
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-21 10:48:31 +0300

    theme: add light & dark colors each in equivalent object

--- app/src/main/java/com/ammaryasser/athar/presentation/theme/Color.kt
+++ app/src/main/java/com/ammaryasser/athar/presentation/theme/Color.kt
@@ -3,12 +3,42 @@ package com.ammaryasser.athar.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 
-val DeepForestGreen = Color(0xFF233F37)
-val SageGreen = Color(0xFF8E9C74)
-val WarmIvory = Color(0xFFF7F2E9)
+object DarkColors {
+    val Charcoal = Color(0xFF232425)
+    val SoftCharcoal = Color(0xFF2B2C2E)
+    val SlateCharcoal = Color(0xFF343537)
+    val CharcoalBorder = Color(0xFF4A4B4D)
+    val SubtleBorder = Color(0xFF393A3C)
+    val WarmWhite = Color(0xFFE8E5DC)
+    val WarmGray = Color(0xFFB9B8B2)
+    val SageGreen = Color(0xFFA9C58F)
+    val PaleSage = Color(0xFFD9E8CA)
+    val DeepSage = Color(0xFF35462F)
+    val DeepMoss = Color(0xFF1B2618)
+    val WarmGold = Color(0xFFD4B56A)
+    val DarkGold = Color(0xFF4A3D23)
+    val SoftGold = Color(0xFFF0DFB5)
+    val DeepBrown = Color(0xFF332915)
+    val MutedTerracotta = Color(0xFFE5A39A)
+    val DeepRed = Color(0xFF3B0D09)
+}
 
-val Surface = Color(0xFFFFFCF8)
-val Divider = Color(0xFFD9D2C5)
-val Accent = Color(0xFFC9A86A)
-val TextPrimary = Color(0xFF233F37)
-val TextSecondary = Color(0xFF6B7368)
+
+object LightColors {
+    val ForestGreen = Color(0xFF315A4A)
+    val White = Color(0xFFFFFFFF)
+    val PaleSage = Color(0xFFD7E6DC)
+    val DeepForest = Color(0xFF1E3D33)
+    val WarmGold = Color(0xFFD8C08A)
+    val DeepBrown = Color(0xFF3C321E)
+    val SoftGold = Color(0xFFF2EBD8)
+    val WarmBrown = Color(0xFF4A402B)
+    val WarmIvory = Color(0xFFF7F5EF)
+    val CharcoalText = Color(0xFF252827)
+    val PaperWhite = Color(0xFFFFFDF9)
+    val SoftWarmGray = Color(0xFFEDECE6)
+    val MutedSageGray = Color(0xFF68706B)
+    val WarmBorder = Color(0xFFD5D5CF)
+    val SubtleBorder = Color(0xFFE3E2DC)
+    val MutedTerracotta = Color(0xFFB85C50)
+}
