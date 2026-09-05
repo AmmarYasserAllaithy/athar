a5baa51ef596929d15a1e5357a914578c9517829
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-09-05 15:20:25 +0300

    bump version to 0.3 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ammaryasser.athar"
         minSdk = 28
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
